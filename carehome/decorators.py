fb14bff14c015ed8bc1f9666df9fb787abea2e0d
tolase <tolasedairo@gmail.com>
2026-02-12 23:09:38 +0000

    Implement role-based access decorator and residents list view according to user stories

---
+++ carehome/decorators.py
@@ -0,0 +1,18 @@
+from django.shortcuts import redirect
+from functools import wraps
+
+
+def role_based_access(allowed_roles=[]):
+    """
+    Restrict access to views based on user role.
+    Example:
+        @role_based_access(['manager', 'senior_carer', 'carer'])
+    """
+    def decorator(view_func):
+        @wraps(view_func)
+        def wrapper(request, *args, **kwargs):
+            if request.user.is_authenticated and request.user.role in allowed_roles:
+                return view_func(request, *args, **kwargs)
+            return redirect('/')  # Unauthorized users go to home
+        return wrapper
+    return decorator
--- carehome/views.py
+++ carehome/views.py
@@ -1,9 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
-
+from .decorators import role_based_access
+from .models import Resident
 
 
 # Create your views here.
 def home(request):
     return HttpResponse("Welcome to Care Home Handover App")
+
+
+@role_based_access(['manager', 'senior_carer', 'carer'])
+def residents_list(request):
+    user_role = request.user.role
+
+    if user_role in ['manager', 'senior_carer']:
+        residents = Resident.objects.all()  # Full info
+    else:
+        # Carers only see limited info
+        residents = Resident.objects.all().only('id', 'first_name', 'last_name', 'room_number')
+
+    context = {
+        'residents': residents,
+        'user_role': user_role
+    }
+    return render(request, 'carehome/residents_list.html', context)
